83ecc5ea3e17afb55088ec5a1c804d4ae94519e2
Donovan Wilder <donovan.wilder@gmail.com>
2022-12-22 06:19:23 -0500

    able to find the position after rotation with findSimpleIndex function

--- src/main/kotlin/CircularArrayRotation.kt
+++ src/main/kotlin/CircularArrayRotation.kt
@@ -1,2 +1,20 @@
-class CircularArrayRotation {
+fun main() {
+    println(findSimpleIndex(4, 3, 3))
+}
+
+
+fun circularArrayRotation(a: Array<Int>, k: Int, queries: Array<Int>): Array<Int> {
+    // Write your code here
+
+    return Array(3) { 3 }
+}
+
+fun findSimpleIndex(sizeOfArray: Int, indexPos: Int, rotations: Int): Int {
+    var newSimpleIndex = indexPos + rotations
+    if(newSimpleIndex > sizeOfArray-1){
+        while(newSimpleIndex >=sizeOfArray-1){
+            newSimpleIndex-= sizeOfArray
+        }
+    }
+    return newSimpleIndex
 }
